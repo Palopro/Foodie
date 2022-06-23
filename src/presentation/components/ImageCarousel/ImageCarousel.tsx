e74bc4ba3ebd883d6dba2d952a3f7b95c34f0e4c
Vladyslav Harkovenko <vladyslav.harkovenko@computools.com>
2022-06-23 13:14:07 +0300

    refactoring code

--- src/presentation/components/ImageCarousel/ImageCarousel.tsx
+++ src/presentation/components/ImageCarousel/ImageCarousel.tsx
@@ -18,9 +18,6 @@ const DEFAULT_INDEX = 0;
 const ACTIVE_COLOR = 'rgb(250, 74, 12)';
 const DEFAULT_COLOR = 'rgb(196, 196, 196)';
 
-const bgColor = (index: number, activeImg: number) =>
-  index === activeImg ? ACTIVE_COLOR : DEFAULT_COLOR;
-
 export const ImageCarousel: React.FC<ImageCarouselProps> = ({ images }) => {
   const [activeImg, setActiveImg] = useState(DEFAULT_INDEX);
 
@@ -42,14 +39,13 @@ export const ImageCarousel: React.FC<ImageCarouselProps> = ({ images }) => {
     }
   };
 
-  const renderDots = () =>
-    images.map((img, index: number) => {
-      let backgroundColor = bgColor(index, activeImg);
+  const renderDot = (index: number) => {
+    const backgroundColor = index === activeImg ? ACTIVE_COLOR : DEFAULT_COLOR;
 
-      return (
-        <Animated.View key={index} style={[styles.dot, { backgroundColor }]} />
-      );
-    });
+    return (
+      <Animated.View key={index} style={[styles.dot, { backgroundColor }]} />
+    );
+  };
 
   return (
     <>
@@ -67,7 +63,9 @@ export const ImageCarousel: React.FC<ImageCarouselProps> = ({ images }) => {
         onScroll={handleScroll}
       />
 
-      <View style={styles.dotView}>{renderDots()}</View>
+      <View style={styles.dotView}>
+        {images.map((img: string, index: number) => renderDot(index))}
+      </View>
     </>
   );
 };
